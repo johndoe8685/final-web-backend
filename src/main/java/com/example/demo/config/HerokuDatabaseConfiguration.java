9ba0425a1f2796e4c20e969e7a999c277fb76443
Berkay Aslan <johndoe8685@protonmail.com>
2025-07-02 22:51:48 +0300

    Heroku deploymnet

--- src/main/java/com/example/demo/config/HerokuDatabaseConfiguration.java
+++ src/main/java/com/example/demo/config/HerokuDatabaseConfiguration.java
@@ -1,13 +1,11 @@
 package com.example.demo.config;
 
-package com.example.demo.config;
-
-import javax.sql.DataSource;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import javax.sql.DataSource;
 import java.net.URI;
 import java.net.URISyntaxException;
 
@@ -17,18 +15,21 @@ public class HerokuDatabaseConfiguration {
 
     @Bean
     public DataSource dataSource() throws URISyntaxException {
-        String databaseUrl = System.getenv("JAWSDB_MARIA_URL");
+        String databaseUrl = System.getenv("CLEARDB_DATABASE_URL");
+
         if (databaseUrl == null) {
-            databaseUrl = System.getenv("CLEARDB_DATABASE_URL");
+            databaseUrl = System.getenv("JAWSDB_MARIA_URL");
         }
+        
         if (databaseUrl == null) {
-            throw new RuntimeException("Veritabanı URL'si bulunamadı. Lütfen JawsDB veya ClearDB eklentisini kurun.");
+            throw new RuntimeException("Veritabanı URL'si (CLEARDB_DATABASE_URL veya JAWSDB_MARIA_URL) bulunamadı. Lütfen Heroku'da veritabanı eklentisini kurduğunuzdan emin olun.");
         }
 
         URI dbUri = new URI(databaseUrl);
 
         String username = dbUri.getUserInfo().split(":")[0];
         String password = dbUri.getUserInfo().split(":")[1];
+
         String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
 
         return DataSourceBuilder.create()
